73806d251ec0791538d8be76dacbafea7bd320b0
andhre143 <rheanbalabat@gmail.com>
2026-05-28 02:27:47 +0800

    merge remote

--- enrollment_system/settings.py
+++ enrollment_system/settings.py
@@ -127,6 +127,31 @@ elif all(
             "PORT": os.environ.get("PGPORT", "5432"),
         }
     }
+elif all(
+    os.environ.get(key)
+    for key in ("POSTGRES_DB", "POSTGRES_USER", "POSTGRES_PASSWORD", "POSTGRES_HOST")
+):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ["POSTGRES_DB"],
+            "USER": os.environ["POSTGRES_USER"],
+            "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+            "HOST": os.environ["POSTGRES_HOST"],
+            "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+        }
+    }
+elif os.environ.get("RAILWAY_ENVIRONMENT"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB", "enrollment_db"),
+            "USER": os.environ.get("POSTGRES_USER", "postgres"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "123"),
+            "HOST": os.environ.get("POSTGRES_HOST", "postgres.railway.internal"),
+            "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+        }
+    }
 else:
     DATABASES = {
         "default": dj_database_url.parse(
