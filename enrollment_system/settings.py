83be63181e83634acc1f30cbea1389ddb7f1773a
andhre143 <rheanbalabat@gmail.com>
2026-05-28 02:31:04 +0800

    merge remote

--- enrollment_system/settings.py
+++ enrollment_system/settings.py
@@ -142,16 +142,11 @@ elif all(
         }
     }
 elif os.environ.get("RAILWAY_ENVIRONMENT"):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB", "enrollment_db"),
-            "USER": os.environ.get("POSTGRES_USER", "postgres"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "123"),
-            "HOST": os.environ.get("POSTGRES_HOST", "postgres.railway.internal"),
-            "PORT": os.environ.get("POSTGRES_PORT", "5432"),
-        }
-    }
+    raise RuntimeError(
+        "Railway PostgreSQL credentials are missing. Add DATABASE_URL from "
+        "your Railway PostgreSQL service to this Django service, for example: "
+        "DATABASE_URL=${{ Postgres.DATABASE_URL }}"
+    )
 else:
     DATABASES = {
         "default": dj_database_url.parse(
